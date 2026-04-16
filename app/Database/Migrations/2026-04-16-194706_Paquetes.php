f5b067a594ad3753eebb3d531f882bbee4c949da
Daniel Ayala <danielayalaromo8@gmail.com>
2026-04-16 15:02:49 -0500

    Se agrego la migracion paquetes

--- app/Database/Migrations/2026-04-16-194706_Paquetes.php
+++ app/Database/Migrations/2026-04-16-194706_Paquetes.php
@@ -8,11 +8,20 @@ class Paquetes extends Migration
 {
     public function up()
     {
-        //
+         $this->forge->addField([
+            'id_paquete'=>['type'=>'INT','auto_increment'=>true],
+            'nombre_paquete'=>['type'=>'VARCHAR','constraint'=>150],
+            'precio_base'=>['type'=>'DECIMAL','constraint'=>'10,2'],
+            'imagen'=>['type'=>'VARCHAR','constraint'=>255,'null'=>true],
+            'detalle_paquete'=>['type'=>'TEXT','null'=>true],
+            'estado'=>['type'=>'VARCHAR','constraint'=>20],
+        ]);
+        $this->forge->addKey('id_paquete', true);
+        $this->forge->createTable('paquetes');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('paquetes');
     }
 }
