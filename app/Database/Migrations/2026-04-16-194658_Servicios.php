affd2a1ac62924dda4aed4b50dcd0840819d732a
Daniel Ayala <danielayalaromo8@gmail.com>
2026-04-16 15:01:00 -0500

    Se agrego la migracion servicios

--- app/Database/Migrations/2026-04-16-194658_Servicios.php
+++ app/Database/Migrations/2026-04-16-194658_Servicios.php
@@ -8,11 +8,18 @@ class Servicios extends Migration
 {
     public function up()
     {
-        //
+         $this->forge->addField([
+            'id_servicio'=>['type'=>'INT','auto_increment'=>true],
+            'nombre_servicio'=>['type'=>'VARCHAR','constraint'=>150],
+            'detalle_servicio'=>['type'=>'TEXT','null'=>true],
+            'estado'=>['type'=>'VARCHAR','constraint'=>20],
+        ]);
+        $this->forge->addKey('id_servicio', true);
+        $this->forge->createTable('servicios');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('servicios');
     }
 }
