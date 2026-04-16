bf7cca833d3415879f7da87d6aa544f075572782
Daniel Ayala <danielayalaromo8@gmail.com>
2026-04-16 14:53:23 -0500

    Se programo la migracion roles

--- app/Database/Migrations/2026-04-16-194637_Roles.php
+++ app/Database/Migrations/2026-04-16-194637_Roles.php
@@ -8,11 +8,16 @@ class Roles extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_rol' => ['type'=>'INT','auto_increment'=>true],
+            'rol' => ['type'=>'VARCHAR','constraint'=>50],
+        ]);
+        $this->forge->addKey('id_rol', true);
+        $this->forge->createTable('roles');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('roles');
     }
 }
