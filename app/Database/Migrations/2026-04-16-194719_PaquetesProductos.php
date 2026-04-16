b091b3b5491f0f0cdfa51caae922f257e4460f82
Daniel Ayala <danielayalaromo8@gmail.com>
2026-04-16 15:06:07 -0500

    Se agrego la migracion paquetes_productos

--- app/Database/Migrations/2026-04-16-194719_PaquetesProductos.php
+++ app/Database/Migrations/2026-04-16-194719_PaquetesProductos.php
@@ -8,11 +8,19 @@ class PaquetesProductos extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_paquete_producto'=>['type'=>'INT','auto_increment'=>true],
+            'id_producto'=>['type'=>'INT'],
+            'id_paquete'=>['type'=>'INT'],
+        ]);
+        $this->forge->addKey('id_paquete_producto', true);
+        $this->forge->addForeignKey('id_producto','productos','id_producto','CASCADE','CASCADE');
+        $this->forge->addForeignKey('id_paquete','paquetes','id_paquete','CASCADE','CASCADE');
+        $this->forge->createTable('paquetes_productos');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('paquetes_productos');
     }
 }
