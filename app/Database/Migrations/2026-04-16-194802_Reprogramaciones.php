c3fe026d706087e5c1fa5a9665e6fa6ae225b932
Daniel Ayala <danielayalaromo8@gmail.com>
2026-04-16 15:09:26 -0500

    Se agrego la migracion reprogramaciones

--- app/Database/Migrations/2026-04-16-194802_Reprogramaciones.php
+++ app/Database/Migrations/2026-04-16-194802_Reprogramaciones.php
@@ -8,11 +8,21 @@ class Reprogramaciones extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_reprogramacion'=>['type'=>'INT','auto_increment'=>true],
+            'fecha_anterior'=>['type'=>'DATETIME'],
+            'fecha_nueva'=>['type'=>'DATETIME'],
+            'motivo'=>['type'=>'TEXT'],
+            'fecha_cambio'=>['type'=>'DATETIME'],
+            'id_contrato'=>['type'=>'INT'],
+        ]);
+        $this->forge->addKey('id_reprogramacion', true);
+        $this->forge->addForeignKey('id_contrato','contratos','id_contrato','CASCADE','CASCADE');
+        $this->forge->createTable('reprogramaciones');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('reprogramaciones');
     }
 }
