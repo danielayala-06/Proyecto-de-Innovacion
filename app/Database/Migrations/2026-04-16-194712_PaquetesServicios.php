de230c67fc7b3d31778e4502cb3569dcc763eb44
Daniel Ayala <danielayalaromo8@gmail.com>
2026-04-16 15:05:18 -0500

    Se agrego la migracion paquetes_servicios

--- app/Database/Migrations/2026-04-16-194712_PaquetesServicios.php
+++ app/Database/Migrations/2026-04-16-194712_PaquetesServicios.php
@@ -8,11 +8,19 @@ class PaquetesServicios extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_paquete_servicio'=>['type'=>'INT','auto_increment'=>true],
+            'id_servicio'=>['type'=>'INT'],
+            'id_paquete'=>['type'=>'INT'],
+        ]);
+        $this->forge->addKey('id_paquete_servicio', true);
+        $this->forge->addForeignKey('id_servicio','servicios','id_servicio','CASCADE','CASCADE');
+        $this->forge->addForeignKey('id_paquete','paquetes','id_paquete','CASCADE','CASCADE');
+        $this->forge->createTable('paquetes_servicios');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('paquetes_servicios');
     }
 }
