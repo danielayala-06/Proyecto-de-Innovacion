1f5c3ffdf32d832e466aab414a4be9272bf6a381
Daniel Ayala <danielayalaromo8@gmail.com>
2026-04-16 14:54:19 -0500

    Se programo la migracion personas

--- app/Database/Migrations/2026-04-16-194641_Personas.php
+++ app/Database/Migrations/2026-04-16-194641_Personas.php
@@ -8,11 +8,22 @@ class Personas extends Migration
 {
     public function up()
     {
-        //
+         $this->forge->addField([
+            'id_persona' => ['type'=>'INT','auto_increment'=>true],
+            'nombres' => ['type'=>'VARCHAR','constraint'=>100],
+            'apellidos' => ['type'=>'VARCHAR','constraint'=>100],
+            'telefono' => ['type'=>'VARCHAR','constraint'=>20,'null'=>true],
+            'correo' => ['type'=>'VARCHAR','constraint'=>150],
+            'tel_alternativo' => ['type'=>'VARCHAR','constraint'=>20,'null'=>true],
+            'numero_documento' => ['type'=>'VARCHAR','constraint'=>50],
+            'tipo_documento' => ['type'=>'VARCHAR','constraint'=>20],
+        ]);
+        $this->forge->addKey('id_persona', true);
+        $this->forge->createTable('personas');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('personas');
     }
 }
