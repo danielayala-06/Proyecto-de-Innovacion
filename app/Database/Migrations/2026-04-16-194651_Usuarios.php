a42b004054b8aac1109cf86737ddaccbf40d8a70
Daniel Ayala <danielayalaromo8@gmail.com>
2026-04-16 14:57:22 -0500

    Se agrego la migracion usuarios

--- app/Database/Migrations/2026-04-16-194651_Usuarios.php
+++ app/Database/Migrations/2026-04-16-194651_Usuarios.php
@@ -8,11 +8,25 @@ class Usuarios extends Migration
 {
     public function up()
     {
-        //
+         $this->forge->addField([
+            'id_usuario' => ['type'=>'INT','auto_increment'=>true],
+            'estado' => ['type'=>'VARCHAR','constraint'=>20],
+            'nom_user' => ['type'=>'VARCHAR','constraint'=>50],
+            'password' => ['type'=>'VARCHAR','constraint'=>255],
+            'tipo_usuario' => ['type'=>'VARCHAR','constraint'=>50],
+            'id_persona' => ['type'=>'INT'],
+            'id_rol' => ['type'=>'INT'],
+        ]);
+
+        $this->forge->addKey('id_usuario', true);
+        $this->forge->addForeignKey('id_persona','personas','id_persona','CASCADE','CASCADE');
+        $this->forge->addForeignKey('id_rol','roles','id_rol','CASCADE','CASCADE');
+
+        $this->forge->createTable('usuarios');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('usuarios');
     }
 }
