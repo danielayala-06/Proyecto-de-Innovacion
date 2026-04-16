764c9ba7584a733d87bdb9c484a7249d95619225
Daniel Ayala <danielayalaromo8@gmail.com>
2026-04-16 15:01:36 -0500

    Se agrego la migracion productos

--- app/Database/Migrations/2026-04-16-194702_Productos.php
+++ app/Database/Migrations/2026-04-16-194702_Productos.php
@@ -8,11 +8,17 @@ class Productos extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_producto'=>['type'=>'INT','auto_increment'=>true],
+            'descripcion'=>['type'=>'VARCHAR','constraint'=>150],
+            'precio_referencial'=>['type'=>'DECIMAL','constraint'=>'10,2'],
+        ]);
+        $this->forge->addKey('id_producto', true);
+        $this->forge->createTable('productos');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('productos');
     }
 }
