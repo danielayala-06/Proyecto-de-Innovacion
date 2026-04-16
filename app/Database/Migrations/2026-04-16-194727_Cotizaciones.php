1cfb6e1fa39e9882ec1eebc56cb04c4f149d4591
Daniel Ayala <danielayalaromo8@gmail.com>
2026-04-16 15:06:31 -0500

    Se agrego la migracion cotizaciones

--- app/Database/Migrations/2026-04-16-194727_Cotizaciones.php
+++ app/Database/Migrations/2026-04-16-194727_Cotizaciones.php
@@ -8,11 +8,29 @@ class Cotizaciones extends Migration
 {
     public function up()
     {
-        //
+         $this->forge->addField([
+            'id_cotizacion'=>['type'=>'INT','auto_increment'=>true],
+            'num_dias_vigencia'=>['type'=>'INT'],
+            'fecha_registro'=>['type'=>'DATE'],
+            'fecha_hora_inicio'=>['type'=>'DATETIME'],
+            'fecha_hora_fin'=>['type'=>'DATETIME'],
+            'direccion'=>['type'=>'VARCHAR','constraint'=>255],
+            'referencia'=>['type'=>'VARCHAR','constraint'=>255,'null'=>true],
+            'latitud'=>['type'=>'DECIMAL','constraint'=>'10,8'],
+            'longitud'=>['type'=>'DECIMAL','constraint'=>'11,8'],
+            'estado'=>['type'=>'VARCHAR','constraint'=>20],
+            'total_estimado'=>['type'=>'DECIMAL','constraint'=>'10,2'],
+            'id_cliente'=>['type'=>'INT'],
+            'id_usuario'=>['type'=>'INT'],
+        ]);
+        $this->forge->addKey('id_cotizacion', true);
+        $this->forge->addForeignKey('id_cliente','clientes','id_cliente','CASCADE','CASCADE');
+        $this->forge->addForeignKey('id_usuario','usuarios','id_usuario','CASCADE','CASCADE');
+        $this->forge->createTable('cotizaciones');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('cotizaciones');
     }
 }
