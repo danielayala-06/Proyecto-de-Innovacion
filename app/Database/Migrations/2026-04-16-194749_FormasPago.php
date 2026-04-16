639bb67ddbb27cfa5f5244ec4139100cc167719d
Daniel Ayala <danielayalaromo8@gmail.com>
2026-04-16 15:08:15 -0500

    Se agrego la migracion formas_pago

--- app/Database/Migrations/2026-04-16-194749_FormasPago.php
+++ app/Database/Migrations/2026-04-16-194749_FormasPago.php
@@ -8,11 +8,17 @@ class FormasPago extends Migration
 {
     public function up()
     {
-        //
+         $this->forge->addField([
+            'id_form_pago'=>['type'=>'INT','auto_increment'=>true],
+            'forma_pago'=>['type'=>'VARCHAR','constraint'=>50],
+            'tipo_pago'=>['type'=>'VARCHAR','constraint'=>50],
+        ]);
+        $this->forge->addKey('id_form_pago', true);
+        $this->forge->createTable('formas_pago');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('formas_pago');
     }
 }
