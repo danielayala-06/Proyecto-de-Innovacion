86c0aed79114d82016f847acbd40c72a839cfed0
Daniel Ayala <danielayalaromo8@gmail.com>
2026-04-17 11:49:56 -0500

    Se agrego el modelo para Paquetes

---
+++ app/Models/Paquete.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Paquete extends Model
+{
+    
+    protected $table            = 'paquetes';
+    protected $primaryKey       = 'id_paquete';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['nombre_paquete', 'precio_base', 'imagen', 'detalle_paquete', 'estado'];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+}
