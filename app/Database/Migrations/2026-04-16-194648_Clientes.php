3cde02f6e45edc70ca0873d1d8af1f91841ef64d
Daniel Ayala <danielayala8@gmail.com>
2026-04-18 12:11:08 -0500

    Se corrigirieron los siguientes puntos en la migracion CLIENTES: > Se agrego clientes.metodo_comunicacion > Se agrego clientes.tipo_cliente > Se agrego clientes.estado > Se cambio la accion de eliminar a RESTRICT en la foranea clientes.id_cliente > Se cambio la accion de eliminar a RESTRICT en la foranea clientes.id_empresa

--- app/Database/Migrations/2026-04-16-194648_Clientes.php
+++ app/Database/Migrations/2026-04-16-194648_Clientes.php
@@ -9,15 +9,47 @@ class Clientes extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id_cliente' => ['type'=>'INT','auto_increment'=>true],
-            'red_social' => ['type'=>'VARCHAR','constraint'=>100,'null'=>true],
-            'id_persona' => ['type'=>'INT'],
-            'id_empresa' => ['type'=>'INT','null'=>true],
+            'id_cliente' => [
+                'type'=>'INT',
+                'unsigned'=>true,
+                'auto_increment'=>true
+            ],
+            'red_social' => [
+                'type'=>'VARCHAR',
+                'constraint'=>150,
+                'null'=>true
+            ],
+            'id_persona' => [
+                'type'=>'INT',
+                'unsigned'=>true,
+                'null'=>false
+            ]
+            ,
+            'id_empresa' => [
+                'type'=>'INT',
+                'unsigned'=>true,
+                'null'=>true,
+            ],
+            'metodo_comunicacion' => [
+                'type'=>'ENUM',
+                'constraint'=>['CORREO', 'WHATSAPP', 'LLAMADAS', 'OTRO'],
+                'null'=>true,
+            ],
+            'tipo_cliente' => [
+                'type'=>'ENUM',
+                'constraint'=>['NUEVO', 'RECURRENTE', 'LEAL'],
+                'default'=>'NUEVO',
+            ],
+            'estado' => [
+                'type'=>'ENUM',
+                'constraint'=>['ACTIVO', 'INACTIVO'],
+                'default'=>'ACTIVO',
+            ],
         ]);
 
         $this->forge->addKey('id_cliente', true);
-        $this->forge->addForeignKey('id_persona','personas','id_persona','CASCADE','CASCADE');
-        $this->forge->addForeignKey('id_empresa','empresas','id_empresa','SET NULL','CASCADE');
+        $this->forge->addForeignKey('id_persona','personas','id_persona','RESTRICT','RESTRICT');
+        $this->forge->addForeignKey('id_empresa','empresas','id_empresa','SET NULL','RESTRICT');
 
         $this->forge->createTable('clientes');
     }
