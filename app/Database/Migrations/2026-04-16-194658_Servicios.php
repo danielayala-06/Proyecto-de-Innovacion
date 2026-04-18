8b9919377f2c35dc1137e531fd67d95840f05bd7
Daniel Ayala <danielayala8@gmail.com>
2026-04-18 12:27:30 -0500

    Se realizaron los siguientes cambios en la migracion SERVICIOS: > Se agrego la restriccion UNSIGNED a la PK. > Se cambio el tipo de dato de VARCHAR a ENUM en la columna servicios.estado

--- app/Database/Migrations/2026-04-16-194658_Servicios.php
+++ app/Database/Migrations/2026-04-16-194658_Servicios.php
@@ -9,10 +9,24 @@ class Servicios extends Migration
     public function up()
     {
          $this->forge->addField([
-            'id_servicio'=>['type'=>'INT','auto_increment'=>true],
-            'nombre_servicio'=>['type'=>'VARCHAR','constraint'=>150],
-            'detalle_servicio'=>['type'=>'TEXT','null'=>true],
-            'estado'=>['type'=>'VARCHAR','constraint'=>20],
+            'id_servicio'=>[
+                'type'=>'INT',
+                'auto_increment'=>true,
+                'unsigned'=>true
+            ],
+            'nombre_servicio'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>150
+            ],
+            'detalle_servicio'=>[
+                'type'=>'TEXT',
+                'null'=>true
+            ],
+            'estado'=>[
+                'type'=>'ENUM',
+                'constraint'=>['ACTIVO','INACTIVO'],
+                'default'=>'ACTIVO'
+            ],
         ]);
         $this->forge->addKey('id_servicio', true);
         $this->forge->createTable('servicios');
