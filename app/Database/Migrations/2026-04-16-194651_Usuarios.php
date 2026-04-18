33665196a439b3e3d2d25d034a429b72c743c013
Daniel Ayala <danielayala8@gmail.com>
2026-04-18 12:23:48 -0500

    Cambios realizados en la migracion USUARIOS: > Se cambio el tipo de dato de VARCHAR a ENUM en la columna usuarios.estado > Se cambio el tipo de dato de VARCHAR a ENUM en la columna usuarios.tipo_usuario > Se agrego la restriccion UNSIGNED a las foraneas

--- app/Database/Migrations/2026-04-16-194651_Usuarios.php
+++ app/Database/Migrations/2026-04-16-194651_Usuarios.php
@@ -9,18 +9,41 @@ class Usuarios extends Migration
     public function up()
     {
          $this->forge->addField([
-            'id_usuario' => ['type'=>'INT','auto_increment'=>true],
-            'estado' => ['type'=>'VARCHAR','constraint'=>20],
-            'nom_user' => ['type'=>'VARCHAR','constraint'=>50],
-            'password' => ['type'=>'VARCHAR','constraint'=>255],
-            'tipo_usuario' => ['type'=>'VARCHAR','constraint'=>50],
-            'id_persona' => ['type'=>'INT'],
-            'id_rol' => ['type'=>'INT'],
+            'id_usuario' => [
+                'type'=>'INT',
+                'auto_increment'=>true,
+                'unsigned'=>true
+            ],
+            'id_persona' => [
+                'type'=>'INT',
+                'unsigned'=>true
+            ],
+            'id_rol' => [
+                'type'=>'INT',
+                'unsigned'=>true
+            ],
+            'nombre_user' => [
+                'type'=>'VARCHAR',
+                'constraint'=>50
+            ],
+            'password' => [
+                'type'=>'VARCHAR',
+                'constraint'=>255
+            ],
+            'tipo_usuario' => [
+                'type'=>'ENUM',
+                'constraint'=>['ADMIN','INVITADO', 'PERSONALIZADO']
+            ],
+            'estado' => [
+                'type'=>'ENUM',
+                'constraint'=>['ACTIVO', 'INACTIVO']
+            ],
         ]);
 
         $this->forge->addKey('id_usuario', true);
-        $this->forge->addForeignKey('id_persona','personas','id_persona','CASCADE','CASCADE');
-        $this->forge->addForeignKey('id_rol','roles','id_rol','CASCADE','CASCADE');
+        $this->forge->addUniqueKey('nombre_user');
+        $this->forge->addForeignKey('id_persona','personas','id_persona','CASCADE','RESTRICT');
+        $this->forge->addForeignKey('id_rol','roles','id_rol','CASCADE','RESTRICT');
 
         $this->forge->createTable('usuarios');
     }
