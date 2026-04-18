37a8c4df2bc62e7dbf00a31dd36c1797b5dedc59
Daniel Ayala <danielayala8@gmail.com>
2026-04-18 11:45:00 -0500

    Se agrego la una UK para DNI en la migracion PERSONAS

--- app/Database/Migrations/2026-04-16-194641_Personas.php
+++ app/Database/Migrations/2026-04-16-194641_Personas.php
@@ -9,16 +9,49 @@ class Personas extends Migration
     public function up()
     {
          $this->forge->addField([
-            'id_persona' => ['type'=>'INT','auto_increment'=>true],
-            'nombres' => ['type'=>'VARCHAR','constraint'=>100],
-            'apellidos' => ['type'=>'VARCHAR','constraint'=>100],
-            'telefono' => ['type'=>'VARCHAR','constraint'=>20,'null'=>true],
-            'correo' => ['type'=>'VARCHAR','constraint'=>150],
-            'tel_alternativo' => ['type'=>'VARCHAR','constraint'=>20,'null'=>true],
-            'numero_documento' => ['type'=>'VARCHAR','constraint'=>50],
-            'tipo_documento' => ['type'=>'VARCHAR','constraint'=>20],
+            'id_persona' => [
+                'type'=>'INT',
+                'unsigned'=>true,
+                'auto_increment'=>true
+            ],
+            'nombres' => [
+                'type'=>'VARCHAR',
+                'constraint'=>100,
+                'null'=>false
+            ],
+            'apellidos' => [
+                'type'=>'VARCHAR',
+                'constraint'=>100,
+                'null'=>true
+            ],
+            'telefono' => [
+                'type'=>'CHAR',
+                'constraint'=>9,
+                'null'=>false
+            ],
+            'correo' => [
+                'type'=>'VARCHAR',
+                'constraint'=>150,
+                'null'=>true
+            ],
+            'tel_alternativo' => [
+                'type'=>'VARCHAR',
+                'constraint'=>20,
+                'null'=>true
+            ],
+            'numero_documento' => [
+                'type'=>'VARCHAR',
+                'constraint'=>50,
+                'null'=>false
+            ],
+            'tipo_documento' => [
+                'type'=>'ENUM',
+                'constraint'=>['DNI', 'CE', 'PASAPORTE'],
+                'null'=>false
+            ],
         ]);
         $this->forge->addKey('id_persona', true);
+        $this->forge->addUniqueKey('numero_documento');
         $this->forge->createTable('personas');
     }
 
