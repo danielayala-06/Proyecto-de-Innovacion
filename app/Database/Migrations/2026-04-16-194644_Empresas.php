92be584c6de10f158e4323bb1d22a053f90e23ad
Daniel Ayala <danielayala8@gmail.com>
2026-04-18 11:55:09 -0500

    Se agregaron las siguientes restricciones a la migracion EMPRESAS: > UK en empresas.ruc > NOT NULL en campos vitales > NULL en campos no tan necesarios > CHAR en empresas.ruc

--- app/Database/Migrations/2026-04-16-194644_Empresas.php
+++ app/Database/Migrations/2026-04-16-194644_Empresas.php
@@ -9,14 +9,39 @@ class Empresas extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id_empresa' => ['type'=>'INT','auto_increment'=>true],
-            'razon_social' => ['type'=>'VARCHAR','constraint'=>150],
-            'ruc' => ['type'=>'VARCHAR','constraint'=>20],
-            'nombre_comercial' => ['type'=>'VARCHAR','constraint'=>150],
-            'telefono' => ['type'=>'VARCHAR','constraint'=>20],
-            'correo' => ['type'=>'VARCHAR','constraint'=>150],
+            'id_empresa' => [
+                'type'=>'INT',
+                'auto_increment'=>true,
+                'unsigned'=>true
+            ],
+            'razon_social' => [
+                'type'=>'VARCHAR',
+                'constraint'=>150,
+                'null'=>false
+            ],
+            'ruc' => [
+                'type'=>'CHAR',
+                'constraint'=>11,
+                'null'=>false
+            ],
+            'nombre_comercial' => [
+                'type'=>'VARCHAR',
+                'constraint'=>150,
+                'false'=>true
+            ],
+            'telefono' => [
+                'type'=>'VARCHAR',
+                'constraint'=>20,
+                'null'=>true
+            ],
+            'correo' => [
+                'type'=>'VARCHAR',
+                'constraint'=>150,
+                'null'=>true
+            ],
         ]);
         $this->forge->addKey('id_empresa', true);
+        $this->forge->addUniqueKey('ruc');
         $this->forge->createTable('empresas');
     }
 
