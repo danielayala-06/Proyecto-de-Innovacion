17955b2c77d29ba580ef3757e6bfddda9abe60d7
Daniel Ayala <danielayala8@gmail.com>
2026-04-17 22:11:51 -0500

    Se agrego el modelo para la vista COTIZACIONES_CLIENTES

---
+++ app/Models/Views/CotizacionClienteModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Views;
+
+use CodeIgniter\Model;
+
+class CotizacionClienteModel extends Model
+{
+    protected $table            = 'cotizacionclientes';
+    protected $primaryKey       = 'id_cotizacion';
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+}
