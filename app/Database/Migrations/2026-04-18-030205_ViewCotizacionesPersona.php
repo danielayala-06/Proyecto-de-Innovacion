719cebe9473b740e400f426f9d42e6349db4be53
Daniel Ayala <danielayala8@gmail.com>
2026-04-17 22:11:11 -0500

    Se agrego la migracion para la vista COTIZACIONES_CLIENTES

---
+++ app/Database/Migrations/2026-04-18-030205_ViewCotizacionesPersona.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class ViewCotizacionesPersona extends Migration
+{
+    public function up()
+    {
+        $this->db->query("
+            CREATE VIEW vw_cotizaciones_detalle AS
+            SELECT 
+                c.id_cotizacion,
+                c.fecha_registro,
+                c.total_estimado,
+                cl.id_cliente,
+                p.nombres,
+                p.apellidos,
+                p.numero_documento,
+                p.telefono
+            FROM Cotizaciones c
+            JOIN Clientes cl ON c.id_cliente = cl.id_cliente
+            JOIN Personas p ON cl.id_persona = p.id_persona
+        ");
+    }
+
+    public function down()
+    {
+        $this->db->query("DROP VIEW IF EXISTS vw_cotizaciones_detalle");
+    }
+}
