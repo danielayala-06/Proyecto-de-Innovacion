ba7fe45526f347d847f6206af70fc30646d04e3e
Daniel Ayala <danielayala8@gmail.com>
2026-04-18 13:00:09 -0500

    Se actualizo la migracion PAQUETES_SERVICIOS:\n > Se elimino la PK por una PK compuesta.\n > Se agregaron restricciones UNSIGNED para las FK.\n > Se actualizo la accion de OnDelete a RESTRICT.\n

--- app/Database/Migrations/2026-04-16-194712_PaquetesServicios.php
+++ app/Database/Migrations/2026-04-16-194712_PaquetesServicios.php
@@ -9,13 +9,18 @@ class PaquetesServicios extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id_paquete_servicio'=>['type'=>'INT','auto_increment'=>true],
-            'id_servicio'=>['type'=>'INT'],
-            'id_paquete'=>['type'=>'INT'],
+            'id_servicio'=>[
+                'type'=>'INT',
+                'unsigned'=>true
+            ],
+            'id_paquete'=>[
+                'type'=>'INT',
+                'unsigned'=>true
+            ],
         ]);
-        $this->forge->addKey('id_paquete_servicio', true);
-        $this->forge->addForeignKey('id_servicio','servicios','id_servicio','CASCADE','CASCADE');
-        $this->forge->addForeignKey('id_paquete','paquetes','id_paquete','CASCADE','CASCADE');
+        $this->forge->addKey(['id_servicio', 'id_paquete'], true);
+        $this->forge->addForeignKey('id_servicio','servicios','id_servicio','CASCADE','RESTRICT');
+        $this->forge->addForeignKey('id_paquete','paquetes','id_paquete','CASCADE','RESTRICT');
         $this->forge->createTable('paquetes_servicios');
     }
 
