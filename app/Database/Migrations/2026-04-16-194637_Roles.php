f8bf915158d61d1fa012a40b98acd3e1bedf4e18
Daniel Ayala <danielayala8@gmail.com>
2026-04-18 11:47:50 -0500

    Se agrego restriccion UK a la migracion ROLES

--- app/Database/Migrations/2026-04-16-194637_Roles.php
+++ app/Database/Migrations/2026-04-16-194637_Roles.php
@@ -9,10 +9,24 @@ class Roles extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id_rol' => ['type'=>'INT','auto_increment'=>true],
-            'rol' => ['type'=>'VARCHAR','constraint'=>50],
+            'id_rol' => [
+                'type'=>'INT',
+                'auto_increment'=>true,
+                'unsigned'=>true
+            ],
+            'rol' => [
+                'type'=>'VARCHAR',
+                'constraint'=>100,
+                'null'=>false
+            ],
+            'estado' => [
+                'type'=>'ENUM',
+                'constraint'=>['ACTIVO','INACTIVO'],
+                'default'=>'ACTIVO'
+            ],
         ]);
         $this->forge->addKey('id_rol', true);
+        $this->forge->addUniqueKey('rol');
         $this->forge->createTable('roles');
     }
 
