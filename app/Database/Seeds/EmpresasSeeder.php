1ca59267db6238223ddfa67ea7d39d99c5738039
Daniel Ayala <danielayala8@gmail.com>
2026-04-18 01:00:35 -0500

    Se agrego correciones a la semilla

--- app/Database/Seeds/EmpresasSeeder.php
+++ app/Database/Seeds/EmpresasSeeder.php
@@ -8,12 +8,28 @@ class EmpresasSeeder extends Seeder
 {
     public function run()
     {
-         $this->db->table('empresas')->insert([
-            'razon_social'=>'Eventos SAC',
-            'ruc'=>'20123456789',
-            'nombre_comercial'=>'EventosPro',
-            'telefono'=>'987654321',
-            'correo'=>'empresa@test.com'
-        ]);
+         $this->db->table('empresas')->insertBatch([
+            [
+                'razon_social'=>'Muebles mariano SAC',
+                'ruc'=>'20907867584',
+                'nombre_comercial'=>'Elmarianito',
+                'telefono'=>'980786798',
+                'correo'=>'elmariano@gmail.com'
+            ],
+            [
+                'razon_social'=>'Colegio nacional fujisoto',
+                'ruc'=>'198970234',
+                'nombre_comercial'=>'El fujisoto',
+                'telefono'=>'908128793',
+                'correo'=>'fuji_soto@gmail.com'
+            ],
+            [
+                'razon_social'=>'Eventos SAC',
+                'ruc'=>'20123456789',
+                'nombre_comercial'=>'EventosPro',
+                'telefono'=>'987654321',
+                'correo'=>'empresa@test.com'
+            ],
+         ]);
     }
 }
