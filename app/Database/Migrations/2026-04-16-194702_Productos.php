c38e6ccd57d95dc1710989eb1a5e56e15f5ca7da
Daniel Ayala <danielayala8@gmail.com>
2026-04-18 12:35:50 -0500

    Se realizaron los siguientes cambios en la migracion PRODUCTOS: > Se agrego la restriccion UNSIGNED a la PK. > Se cambio el tipo de dato de VARCHAR(150) a TEXT en la columna productos.detalle. > Se agrego la columna UNIDAD. > Se elimino la columna PRECIO_REFERENCIAL. > Se cambio el tipo de dato de VARCHAR a ENUM en la columna productos.estado

--- app/Database/Migrations/2026-04-16-194702_Productos.php
+++ app/Database/Migrations/2026-04-16-194702_Productos.php
@@ -9,12 +9,37 @@ class Productos extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id_producto'=>['type'=>'INT','auto_increment'=>true],
-            'nombre'=>['type'=>'VARCHAR','constraint'=>150, 'null'=>false],
-            'descripcion'=>['type'=>'VARCHAR','constraint'=>150, 'null'=>true],
-            'tamanio'=>['type'=>'VARCHAR','constraint'=>150, 'null'=>true],
-            'precio_referencial'=>['type'=>'DECIMAL','constraint'=>'10,2'],
+            'id_producto'=>[
+                'type'=>'INT',
+                'auto_increment'=>true,
+                'unsigned'=>true
+            ],
+            'nombre'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>150,
+                'null'=>false
+            ],
+            'detalle'=>[
+                'type'=>'TEXT',
+                'null'=>true
+            ],
+            'tamanio'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>150,
+                'null'=>true
+            ],
+            'unidad'=>[
+                'type'=>'VARCHAR',
+                'constraint'=>50,
+                'null'=>false
+            ],
+            'estado'=>[
+                'type'=>'ENUM',
+                'constraint'=>['ACTIVO','INACTIVO'],
+                'default'=>'ACTIVO'
+            ],
         ]);
+
         $this->forge->addKey('id_producto', true);
         $this->forge->createTable('productos');
     }
