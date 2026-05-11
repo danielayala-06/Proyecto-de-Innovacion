75fe9caf92e5389fbf3c37709991185fa791853d
Diggy Félix <diggytonyfelixtipacti@gmail.com>
2026-05-11 17:31:27 -0500

    Añadiendo la API decolecta al proyecto

--- public/js/api/cliente.api.js
+++ public/js/api/cliente.api.js
@@ -1,7 +1,8 @@
 import { http } from '../utils/http.js';
 
 export const clienteApi = {
-    listar: ()       => http.get('api/clientes'),
-    obtener: (id)    => http.get(`api/clientes/${id}`),
-    crear:  (data)   => http.post('api/clientes', data),
+    listar:    ()      => http.get('api/clientes'),
+    obtener:   (id)    => http.get(`api/clientes/${id}`),
+    crear:     (data)  => http.post('api/clientes', data),
+    reniecDni: (dni)   => http.get('api/reniec/dni', { numero: dni }),
 };
